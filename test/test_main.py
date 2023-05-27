e3c9dfa699b0ca63492b44b6e108afc612b1a17e
pollenjp <polleninjp@gmail.com>
2023-05-27 12:51:20 +0900

    Add fastapi sample

--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,13 @@
+# Third Party Library
+from fastapi.testclient import TestClient
+
 # First Party Library
-from main import hello_world
+from main import app
+
+client = TestClient(app)
 
 
-def test_hello_world() -> None:
-    assert hello_world() == "Hello World!"
+def test_read_main() -> None:
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
